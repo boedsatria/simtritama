b58fd0e24b0c32b2ffd0885b633aa00dd24f8295
boedsatria <stribez@gmail.com>
2021-10-26 14:25:06 +0700

    header

--- application/views/header.php
+++ application/views/header.php
@@ -3,14 +3,14 @@
     <!-- BEGIN: Head -->
     <head>
         <meta charset="utf-8">
-        <link href="dist/images/logo.svg" rel="shortcut icon">
+        <link href="<?= base_url(); ?>dist/images/logo.svg" rel="shortcut icon">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="Sistem Management Project dan Keuangan PT.BMT">
         <meta name="keywords" content="Sistem informasi BMT">
         <meta name="author" content="SATRIACORP.ID">
         <title>Dashboard</title>
         <!-- BEGIN: CSS Assets-->
-        <link rel="stylesheet" href="dist/css/app.css" />
+        <link rel="stylesheet" href="<?= base_url(); ?>dist/css/app.css" />
         <!-- END: CSS Assets-->
     </head>
     <!-- END: Head -->
