02f760b6fb5f4d754adee2f58a419ab9ee92b226
boedsatria <stribez@gmail.com>
2021-10-26 14:21:22 +0700

    footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,4 @@
-</div>
+        </div>
         <!-- BEGIN: JS Assets-->
         <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBG7gNHAhDzgYmq4-EHvM4bqW1DNj2UCuk&libraries=places"></script>
