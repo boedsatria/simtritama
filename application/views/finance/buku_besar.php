4bb209a2991ff0e3e12ae0b986f8a4ce698d57ed
herupurwantobdg <herupurwanto@gmx.us>
2021-10-27 19:03:18 +0700

    input transaksi

--- application/views/finance/buku_besar.php
+++ application/views/finance/buku_besar.php
@@ -1,24 +1,31 @@
 <!-- BEGIN : JUDUL -->
 <div class="intro-y flex items-center mt-8">
     <h2 class="text-lg font-medium mr-auto">
-        JUDUL PAGE
+        Input Data Buku Besar
         <!--JUDUL PAGE-->
     </h2>
 </div>
 <!-- END JUDUL-->
 <div class="grid grid-cols-12 gap-6 mt-5">
     <!-- KOLOM BESAR-->
-    <div class="intro-y col-span-12 lg:col-span-6">
+    <div class="intro-y col-span-6 lg:col-span-6">
         <!-- KOLOM KIRI-->
         <!-- BEGIN: BLOCK 1 -->
         <div class="intro-y box">
             <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
                 <h2 class="font-medium text-base mr-auto">
-                    BLOK 1
+                    Pilih Kategori
                     <!-- JUDUL BLOK KIRI-->
                 </h2>
-                <!-- KONTEN BLOCK 1 DISINI-->
             </div>
+            <div class="mt-2">
+                <select data-placeholder="Select your favorite actors" class="tom-select w-full">
+                    <option value="1">1000 Harta</option>
+                    <option value="2">2000 Kewajiban</option>
+                    <option value="3">3000 Modal</option>
+                    <option value="4">4000 Pendapatan</option>
+                    <option value="5">6000 Beban Usaha</option>
+                </select> </div>
         </div>
         <!-- END: BLOCK 1 -->
         <br>
@@ -26,11 +33,18 @@
         <div class="intro-y box">
             <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
                 <h2 class="font-medium text-base mr-auto">
-                    BLOK 2
+                    Pilih Nama Transaksi
                     <!-- JUDUL BLOK KIRI-->
                 </h2>
                 <!-- KONTEN BLOCK 2 DISINI-->
             </div>
+            <div class="mt-2"> <select data-placeholder="Select your favorite actors" class="tom-select w-full">
+             <option value="1">1102 BRI</option>
+             <option value="2">1103 Piutang Usaha</option>
+             <option value="3">1104 Piutang Karyawan</option>
+             <option value="4">1105 BCA</option>
+             <option value="5">1106 Inventaris</option>
+         </select> </div>
         </div>
         <!-- END: BLOCK 2 -->
         <br>
@@ -38,13 +52,19 @@
         <div class="intro-y box">
             <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
                 <h2 class="font-medium text-base mr-auto">
-                    BLOK 3
+                    Tentukan Tanggal Transaksi
                     <!-- JUDUL BLOK KIRI-->
                 </h2>
                 <!-- KONTEN BLOCK 3 DISINI-->
             </div>
+                <div class="relative w-56 mx-auto">
+                    <div class="absolute rounded-l w-10 h-full flex items-center justify-center bg-gray-100 border text-gray-600 dark:bg-dark-1 dark:border-dark-4">
+                        <i data-feather="calendar" class="w-4 h-4"></i>
+                    </div> <input type="text" class="datepicker form-control pl-12" data-single-mode="true">
+                </div> 
         </div>
         <!-- END: BLOCK 3 -->
+        <br>
     </div>
     <div class="intro-y col-span-12 lg:col-span-6">
         <!-- KOLOM KANAN-->
@@ -52,38 +72,48 @@
         <div class="intro-y box">
             <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
                 <h2 class="font-medium text-base mr-auto">
-                    BLOK 4
+                    Pilih Tipe Transaksi
                     <!-- JUDUL BLOK 4-->
                 </h2>
+            </div>
+                    <div  class="intro-y col-span-12 lg:col-span-6">
+                        <div class="flex flex-col sm:flex-row mt-1"><br>
+                            <div class="form-check mr-7"> <input id="checkbox-switch-4" class="form-check-input" type="checkbox" value="">
+                                <label class="form-check-label" for="checkbox-switch-4">PEMASUKAN</label>
+                            </div><br>
+                            <div class="form-check mr-2 mt-2 sm:mt-0"> <input id="checkbox-switch-5" class="form-check-input" type="checkbox" value="">
+                                <label class="form-check-label" for="checkbox-switch-5">PENGELUARAN</label>
+                            </div>
+                    </div>
             </div>
         </div>
         <!-- END: BLOCK 4 -->
         <br>
-        <!-- BEGIN: BLOCK 5 -->
+        <!-- BEGIN: BLOCK 4 -->
         <div class="intro-y box">
             <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
                 <h2 class="font-medium text-base mr-auto">
-                    BLOK 5
+                    Input Jumlah Transaksi
                     <!-- JUDUL BLOK 4-->
                 </h2>
             </div>
+            <div><input id="regular-form-1" type="text" class="form-control" placeholder="Input text"> </div>
         </div>
-        <!-- END: BLOCK 5 -->
+        <!-- END: BLOCK 4 -->
+        <br>
+        
         <!-- BEGIN: MENU -->
         <div class="intro-y box mt-5">
             <div id="horizontal-form" class="p-5">
                 <div class="preview">
-                    <button class="btn btn-primary w-32 mr-2 mb-2"> <i data-feather="hard-drive" class="w-4 h-4 mr-2"></i> Simpan </button> <button class="btn btn-danger w-32 mr-2 mb-2"> <i data-feather="trash" class="w-4 h-4 mr-2"></i> Hapus </button> <button class="btn btn-warning w-32 mr-2 mb-2"> <i data-feather="book" class="w-4 h-4 mr-2"></i> New </button> <button class="btn btn-success w-32 mr-2 mb-2"> <i data-feather="printer" class="w-4 h-4 mr-2"></i> Print </button>
-                </div>
-                <div class="source-code hidden">
-                    <button data-target="#copy-horizontal-form" class="copy-code btn py-1 px-2 btn-outline-secondary"> <i data-feather="file" class="w-4 h-4 mr-2"></i> Copy example code </button>
-                    <div class="overflow-y-auto mt-3 rounded-md">
-                        <pre id="copy-horizontal-form" class="source-preview"> <code class="text-xs p-0 rounded-md html pl-5 pt-8 pb-4 -mb-10 -mt-10"> HTMLOpenTagdiv class=&quot;form-inline&quot;HTMLCloseTag HTMLOpenTaglabel for=&quot;horizontal-form-1&quot; class=&quot;form-label sm:w-20&quot;HTMLCloseTagEmailHTMLOpenTag/labelHTMLCloseTag HTMLOpenTaginput id=&quot;horizontal-form-1&quot; type=&quot;text&quot; class=&quot;form-control&quot; placeholder=&quot;example@gmail.com&quot;HTMLCloseTag HTMLOpenTag/divHTMLCloseTag HTMLOpenTagdiv class=&quot;form-inline mt-5&quot;HTMLCloseTag HTMLOpenTaglabel for=&quot;horizontal-form-2&quot; class=&quot;form-label sm:w-20&quot;HTMLCloseTagPasswordHTMLOpenTag/labelHTMLCloseTag HTMLOpenTaginput id=&quot;horizontal-form-2&quot; type=&quot;password&quot; class=&quot;form-control&quot; placeholder=&quot;secret&quot;HTMLCloseTag HTMLOpenTag/divHTMLCloseTag HTMLOpenTagdiv class=&quot;form-check sm:ml-20 sm:pl-5 mt-5&quot;HTMLCloseTag HTMLOpenTaginput id=&quot;horizontal-form-3&quot; class=&quot;form-check-input&quot; type=&quot;checkbox&quot; value=&quot;&quot;HTMLCloseTag HTMLOpenTaglabel class=&quot;form-check-label&quot; for=&quot;horizontal-form-3&quot;HTMLCloseTagRemember meHTMLOpenTag/labelHTMLCloseTag HTMLOpenTag/divHTMLCloseTag HTMLOpenTagdiv class=&quot;sm:ml-20 sm:pl-5 mt-5&quot;HTMLCloseTag HTMLOpenTagbutton class=&quot;btn btn-primary&quot;HTMLCloseTagLoginHTMLOpenTag/buttonHTMLCloseTag HTMLOpenTag/divHTMLCloseTag </code> </pre>
-                    </div>
+                    <button class="btn btn-primary w-32 mr-2 mb-2"> <i data-feather="hard-drive" class="w-4 h-4 mr-2"></i> Simpan </button>
+                    <button class="btn btn-danger w-32 mr-2 mb-2"> <i data-feather="trash" class="w-4 h-4 mr-2"></i> Batal </button>
+                    <button class="btn btn-warning w-32 mr-2 mb-2"> <i data-feather="book" class="w-4 h-4 mr-2"></i> Laporan </button>
+                    <button class="btn btn-success w-32 mr-2 mb-2"> <i data-feather="printer" class="w-4 h-4 mr-2"></i> Print </button>
                 </div>
             </div>
         </div>
-        <!-- END: OPERATION -->
+        <!-- END: menu -->
     </div>
 </div>
 </div>
