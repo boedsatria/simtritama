5c3fbcce90da303bd82acc66c953005733b6c1cc
herupurwantobdg <herupurwanto@gmx.us>
2021-10-28 20:31:51 +0700

    pic production order

--- application/views/pic/media_order.php
+++ application/views/pic/media_order.php
@@ -1,7 +1,7 @@
 <!-- BEGIN : JUDUL -->
 <div class="intro-y flex items-center mt-8">
     <h2 class="text-lg font-medium mr-auto">
-        JUDUL PAGE
+        GENERATE ORDER PRODUKSI DAN ORDER MEDIA
         <!--JUDUL PAGE-->
     </h2>
 </div>
@@ -14,11 +14,53 @@
         <div class="intro-y box">
             <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
                 <h2 class="font-medium text-base mr-auto">
-                    HEADER
+                    PILIH DATA RINGKOS
                 </h2>
             </div>
             <div class="p-5">
-                KONTEN
+                RINGKOS
+ 
+ <!-- BEGIN: Nested Select -->
+ <div class="mt-3"> <label>Direktorat</label>
+     <div class="mt-2"> <select data-placeholder="Select your favorite actors" class="tom-select w-full">
+             <optgroup label="Kementerian A">
+                 <option value="1">Direktorat 1</option>
+                 <option value="2">Direktorat 2</option>
+                 <option value="3">Direktorat 3</option>
+                 <option value="4">Direktorat 4</option>
+                 <option value="5">Direktorat 5</option>
+             </optgroup>
+             <optgroup label="Kementerian B">
+                 <option value="1">Direktorat 1</option>
+                 <option value="2">Direktorat 2</option>
+                 <option value="3">Direktorat 3</option>
+                 <option value="4">Direktorat 4</option>
+                 <option value="5">Direktorat 5</option>
+             </optgroup>
+         </select> </div>
+ </div>
+ <!-- END: Nested Select -->
+  <!-- BEGIN: Nested Select -->
+  <div class="mt-3"> <label>Nomor Ringkos</label>
+     <div class="mt-2"> <select data-placeholder="Select your favorite actors" class="tom-select w-full">
+             <optgroup label="Januari">
+                 <option value="1">Ringkos A101</option>
+                 <option value="2">Ringkos A102</option>
+                 <option value="3">Ringkos A103</option>
+                 <option value="4">Ringkos A104</option>
+                 <option value="5">Ringkos A105</option>
+             </optgroup>
+             <optgroup label="Februari">
+                 <option value="1">Ringkos B001</option>
+                 <option value="2">Ringkos B002</option>
+                 <option value="3">Ringkos B003</option>
+                 <option value="4">Ringkos B004</option>
+                 <option value="5">Ringkos B005</option>
+             </optgroup>
+         </select> </div>
+ </div>
+ <!-- END: Nested Select -->
+
             </div>
         </div>
         <!-- END: BLOCK 1 -->
@@ -27,11 +69,20 @@
         <div class="intro-y box">
             <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
                 <h2 class="font-medium text-base mr-auto">
-                    HEADER
+                    DATA PEKERJAAN
                 </h2>
             </div>
             <div class="p-5">
-                KONTEN
+                WAKTU PEKERJAAN
+                <div class="mt-3"> <label for="regular-form-5" class="form-label">Mulai Produksi</label>
+                <input id="regular-form-5" type="text" class="form-control" placeholder="Otomatis dari database timeline Ringkos" disabled> </div>
+                <div class="mt-3"> <label for="regular-form-5" class="form-label">Batas akhir Produksi</label>
+                <input id="regular-form-5" type="text" class="form-control" placeholder="Otomatis dari database timeline Ringkos" disabled> </div>
+                <div class="mt-3"> <label for="regular-form-5" class="form-label">Mulai Placement</label>
+                <input id="regular-form-5" type="text" class="form-control" placeholder="Otomatis dari database timeline Ringkos" disabled> </div>
+                <div class="mt-3"> <label for="regular-form-5" class="form-label">Batas akhir Placement</label>
+                <input id="regular-form-5" type="text" class="form-control" placeholder="Otomatis dari database timeline Ringkos" disabled> </div>
+                 
             </div>
         </div>
         <!-- END: BLOCK 2 -->
@@ -40,11 +91,15 @@
         <div class="intro-y box">
             <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
                 <h2 class="font-medium text-base mr-auto">
-                    HEADER
+                    ANGGARAN PEKERJAAN
                 </h2>
             </div>
             <div class="p-5">
-                KONTEN
+                HPS Pagu Anggaran Pekerjaan
+                <div class="mt-3"> <label for="regular-form-5" class="form-label">HPS Produksi</label>
+                <input id="regular-form-5" type="text" class="form-control" placeholder="HPS sesuai Pagu Pekerjaan Otomatis dari database Ringkos" disabled> </div>
+                <div class="mt-3"> <label for="regular-form-5" class="form-label">HPS Placement</label>
+                <input id="regular-form-5" type="text" class="form-control" placeholder="HPS sesuai Pagu Pekerjaan Otomatis dari database Ringkos" disabled> </div>
             </div>
         </div>
         <!-- END: BLOCK 3 -->
@@ -55,24 +110,63 @@
         <div class="intro-y box">
             <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
                 <h2 class="font-medium text-base mr-auto">
-                    HEADER
+                    JOB ORDER PRODUKSI
                 </h2>
             </div>
             <div class="p-5">
-                KONTEN
-            </div>
-        </div>
+                Konten Produksi
+                <div class="mt-3"> <label for="regular-form-5" class="form-label">Jumlah Versi</label>
+                <input id="regular-form-5" type="text" class="form-control" placeholder="jumlah versi produksi otomatis dari data Ringkos" disabled> </div>
+                <div class="mt-3"> <label for="regular-form-5" class="form-label">Wilayah Pekerjaan</label>
+                <input id="regular-form-5" type="text" class="form-control" placeholder="otomatis sesuai data Ringkos" disabled> </div>
+                 <!-- BEGIN: Nested Select -->
+ <div class="mt-3"> <label>Tim Pelaksana Produksi</label>
+     <div class="mt-2"> <select data-placeholder="Select your favorite actors" class="tom-select w-full">
+             <optgroup label="Jakarta">
+                 <option value="1">TIM 1</option>
+                 <option value="2">TIM 2</option>
+                 <option value="3">TIM 3</option>
+                 <option value="4">TIM 4</option>
+                 <option value="5">TIM 5</option>
+             </optgroup>
+             <optgroup label="Jawa Barat">
+                 <option value="1">TIM 6</option>
+                 <option value="2">TIM 7</option>
+                 <option value="3">TIM 8</option>
+                 <option value="4">TIM 9</option>
+                 <option value="5">TIM 10</option>
+             </optgroup>
+         </select> </div>
+ </div>
+ <!-- END: Nested Select -->
+            <div class="mt-3"> <label>Upload Materi Produksi</label>
+                <form action="/file-upload" class="dropzone">
+                    <div class="fallback"> <input name="file" type="file" multiple/> </div>
+                    <div class="dz-message" data-dz-message>
+                        <div class="text-lg font-medium">Drop files here or click to upload.</div>
+                        <div class="text-gray-600"> This is just a demo dropzone. Selected files are <span class="font-medium">not</span> actually uploaded. </div>
+                    </div>
+                </form> 
+            </div><br>
+
+
+            <button class="btn btn-success w-32 mr-2 mb-2"> <i data-feather="printer" class="w-4 h-4 mr-2"></i> Print </button>
+            <button class="btn btn-warning w-32 mr-2 mb-2"> <i data-feather="send" class="w-4 h-4 mr-2"></i> Kirim </button>
+
+
+    </div>
+</div>
         <!-- END: BLOCK 4 -->
         <br>
         <!-- BEGIN: BLOCK 5 -->
         <div class="intro-y box">
             <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
                 <h2 class="font-medium text-base mr-auto">
-                    HEADER
+                    JOB ORDER MEDIA
                 </h2>
             </div>
             <div class="p-5">
-                KONTEN
+                Konten Media
             </div>
         </div>
         <!-- END: BLOCK 5 -->
