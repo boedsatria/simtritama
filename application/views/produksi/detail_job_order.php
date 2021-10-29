9bb85d6b6a5a546fadb47db590de5893bb452570
Rizkyrizt212 <75552121+Rizkyrizt212@users.noreply.github.com>
2021-10-29 22:22:42 +0700

    ok

--- application/views/produksi/detail_job_order.php
+++ application/views/produksi/detail_job_order.php
@@ -1,116 +1,118 @@
 <!-- BEGIN : JUDUL -->
 <div class="intro-y flex items-center mt-8">
     <h2 class="text-lg font-medium mr-auto">
-        Detail Job Order Produksi <!--JUDUL PAGE-->
+        Detail Job Order Produksi
+        <!--JUDUL PAGE-->
     </h2>
 </div>
 <!-- END JUDUL-->
-<div class="grid grid-cols-2 gap-2 mt-5"> <!-- KOLOM BESAR-->
-    <div class="intro-y col-span-2 lg:col-span-4"> <!-- KOLOM KIRI-->
+<div class="grid grid-cols-2 gap-2 mt-5">
+    <!-- KOLOM BESAR-->
+    <div class="intro-y col-span-2 lg:col-span-4">
+        <!-- KOLOM KIRI-->
         <!-- BEGIN: BLOCK 1 -->
-            <div class="intro-y box">
-                <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
+        <div class="intro-y box">
+            <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
 
-                    <!-- KONTEN BLOCK 1 DISINI-->
+                <!-- KONTEN BLOCK 1 DISINI-->
 
-                    <div class="overflow-x-auto">
-                        <table class="table">
-                            <thead>
-                                <tr class="bg-gray-700 dark:bg-dark-1 text-white">
-                                    <th class="whitespace-nowrap">#</th>
-                                    <th class="whitespace-nowrap">Data</th>
-                                    <th class="whitespace-nowrap">Value</th>
-                                    <th class="whitespace-nowrap">Deskripsi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td class="border-b dark:border-dark-5">1</td>
-                                    <td class="border-b dark:border-dark-5">Nomor Ringkos</td>
-                                    <td class="border-b dark:border-dark-5">DIR001/001/001/001</td>
-                                    <td class="border-b dark:border-dark-5"> - </td>
-                                </tr>
-                                <tr>
-                                    <td class="border-b dark:border-dark-5">2</td>
-                                    <td class="border-b dark:border-dark-5">Direktorat</td>
-                                    <td class="border-b dark:border-dark-5">Kementerian Tenaga Kerja Republik Indonesia</td>
-                                    <td class="border-b dark:border-dark-5"> - </td>
-                                </tr>
-                                <tr>
-                                    <td class="border-b dark:border-dark-5">3</td>
-                                    <td class="border-b dark:border-dark-5">Tanggal Mulai Produksi</td>
-                                    <td class="border-b dark:border-dark-5">01 Januari 2022</td>
-                                    <td class="border-b dark:border-dark-5"> - </td>
-                                </tr>
-                                <tr>
-                                    <td class="border-b dark:border-dark-5">4</td>
-                                    <td class="border-b dark:border-dark-5">Tanggal Akhir Produksi</td>
-                                    <td class="border-b dark:border-dark-5">01 Maret 2022</td>
-                                    <td class="border-b dark:border-dark-5"> - </td>
-                                </tr>
-                                <tr>
-                                    <td class="border-b dark:border-dark-5">5</td>
-                                    <td class="border-b dark:border-dark-5">Jumlah Versi Produksi</td>
-                                    <td class="border-b dark:border-dark-5">120</td>
-                                    <td class="border-b dark:border-dark-5"> - </td>
-                                </tr>
-                                <tr>
-                                    <td class="border-b dark:border-dark-5">6</td>
-                                    <td class="border-b dark:border-dark-5">Jenis Produksi</td>
-                                    <td class="border-b dark:border-dark-5">Animasi 3D</td>
-                                    <td class="border-b dark:border-dark-5"> 40 </td>
-                                </tr>
-                                <tr>
-                                    <td class="border-b dark:border-dark-5">7</td>
-                                    <td class="border-b dark:border-dark-5">Lokasi Produksi</td>
-                                    <td class="border-b dark:border-dark-5">Jakarta</td>
-                                    <td class="border-b dark:border-dark-5"> - </td>
-                                </tr>
-                                <tr>
-                                    <td class="border-b dark:border-dark-5">8</td>
-                                    <td class="border-b dark:border-dark-5">Jenis Produksi</td>
-                                    <td class="border-b dark:border-dark-5">Animasi 2D</td>
-                                    <td class="border-b dark:border-dark-5"> 40 </td>
-                                </tr>
-                                <tr>
-                                    <td class="border-b dark:border-dark-5">9</td>
-                                    <td class="border-b dark:border-dark-5">Lokasi Produksi</td>
-                                    <td class="border-b dark:border-dark-5">Jakarta</td>
-                                    <td class="border-b dark:border-dark-5"> - </td>
-                                </tr>
-                                <tr>
-                                    <td class="border-b dark:border-dark-5">10</td>
-                                    <td class="border-b dark:border-dark-5">Jenis Produksi</td>
-                                    <td class="border-b dark:border-dark-5">Video Syuting</td>
-                                    <td class="border-b dark:border-dark-5"> Video Syuting dengan Talent Nasional Grade B Durasi 3 Menit </td>
-                                </tr>
-                                <tr>
-                                    <td class="border-b dark:border-dark-5">11</td>
-                                    <td class="border-b dark:border-dark-5">Lokasi Produksi</td>
-                                    <td class="border-b dark:border-dark-5">Jawa Barat</td>
-                                    <td class="border-b dark:border-dark-5"> - </td>
-                                </tr>
-                                <tr>
-                                    <td class="border-b dark:border-dark-5">12</td>
-                                    <td class="border-b dark:border-dark-5">Pagu Biaya Produksi</td>
-                                    <td class="border-b dark:border-dark-5">450.000.000,00</td>
-                                    <td class="border-b dark:border-dark-5"> Rupiah </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                <div class="overflow-x-auto">
+                    <table class="table">
+                        <thead>
+                            <tr class="bg-gray-700 dark:bg-dark-1 text-white">
+                                <th class="whitespace-nowrap">#</th>
+                                <th class="whitespace-nowrap">Data</th>
+                                <th class="whitespace-nowrap">Value</th>
+                                <th class="whitespace-nowrap">Deskripsi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">1</td>
+                                <td class="border-b dark:border-dark-5">Nomor Ringkos</td>
+                                <td class="border-b dark:border-dark-5">DIR001/001/001/001</td>
+                                <td class="border-b dark:border-dark-5"> - </td>
+                            </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">2</td>
+                                <td class="border-b dark:border-dark-5">Direktorat</td>
+                                <td class="border-b dark:border-dark-5">Kementerian Tenaga Kerja Republik Indonesia</td>
+                                <td class="border-b dark:border-dark-5"> - </td>
+                            </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">3</td>
+                                <td class="border-b dark:border-dark-5">Tanggal Mulai Produksi</td>
+                                <td class="border-b dark:border-dark-5">01 Januari 2022</td>
+                                <td class="border-b dark:border-dark-5"> - </td>
+                            </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">4</td>
+                                <td class="border-b dark:border-dark-5">Tanggal Akhir Produksi</td>
+                                <td class="border-b dark:border-dark-5">01 Maret 2022</td>
+                                <td class="border-b dark:border-dark-5"> - </td>
+                            </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">5</td>
+                                <td class="border-b dark:border-dark-5">Jumlah Versi Produksi</td>
+                                <td class="border-b dark:border-dark-5">120</td>
+                                <td class="border-b dark:border-dark-5"> - </td>
+                            </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">6</td>
+                                <td class="border-b dark:border-dark-5">Jenis Produksi</td>
+                                <td class="border-b dark:border-dark-5">Animasi 3D</td>
+                                <td class="border-b dark:border-dark-5"> 40 </td>
+                            </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">7</td>
+                                <td class="border-b dark:border-dark-5">Lokasi Produksi</td>
+                                <td class="border-b dark:border-dark-5">Jakarta</td>
+                                <td class="border-b dark:border-dark-5"> - </td>
+                            </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">8</td>
+                                <td class="border-b dark:border-dark-5">Jenis Produksi</td>
+                                <td class="border-b dark:border-dark-5">Animasi 2D</td>
+                                <td class="border-b dark:border-dark-5"> 40 </td>
+                            </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">9</td>
+                                <td class="border-b dark:border-dark-5">Lokasi Produksi</td>
+                                <td class="border-b dark:border-dark-5">Jakarta</td>
+                                <td class="border-b dark:border-dark-5"> - </td>
+                            </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">10</td>
+                                <td class="border-b dark:border-dark-5">Jenis Produksi</td>
+                                <td class="border-b dark:border-dark-5">Video Syuting</td>
+                                <td class="border-b dark:border-dark-5"> Video Syuting dengan Talent Nasional Grade B Durasi 3 Menit </td>
+                            </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">11</td>
+                                <td class="border-b dark:border-dark-5">Lokasi Produksi</td>
+                                <td class="border-b dark:border-dark-5">Jawa Barat</td>
+                                <td class="border-b dark:border-dark-5"> - </td>
+                            </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">12</td>
+                                <td class="border-b dark:border-dark-5">Pagu Biaya Produksi</td>
+                                <td class="border-b dark:border-dark-5">450.000.000,00</td>
+                                <td class="border-b dark:border-dark-5"> Rupiah </td>
+                            </tr>
+                        </tbody>
+                    </table>
 
-                        <br>
-                <button class="btn btn-primary w-48 mr-2 mb-2"> <i data-feather="printer" class="w-4 h-4 mr-2"></i> Print </button>
-                <button class="btn btn-success w-48 mr-2 mb-2"> <i data-feather="hard-drive" class="w-4 h-4 mr-2"></i> Download Materi </button>
-                <a href="<?= base_url('produksi/pengajuan_produksi') ?>" class="btn btn-primary w-48 mr-2 mb-2"> <i data-feather="search" class="w-4 h-4 mr-2"></i>
-                                    Pengajuan HPS </a>
-                    </div> 
-
-
-
+                    <br>
+                    <button class="btn btn-primary w-48 mr-2 mb-2"> <i data-feather="printer" class="w-4 h-4 mr-2"></i> Print </button>
+                    <button class="btn btn-success w-48 mr-2 mb-2"> <i data-feather="hard-drive" class="w-4 h-4 mr-2"></i> Download Materi </button>
+                    <a href="<?= base_url('produksi/pengajuan') ?>" class="btn btn-primary w-48 mr-2 mb-2"> <i data-feather="search" class="w-4 h-4 mr-2"></i>
+                        Pengajuan HPS </a>
                 </div>
+
+
+
             </div>
+        </div>
         <!-- END: BLOCK 1 -->
     </div>
 </div>
-                        
