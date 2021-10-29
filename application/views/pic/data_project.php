81ab93ad33e4bd38ccfa829b7ba2fde246fae086
herupurwantobdg <herupurwanto@gmx.us>
2021-10-29 14:06:03 +0700

    tambah info produksi di data project

--- application/views/pic/data_project.php
+++ application/views/pic/data_project.php
@@ -111,7 +111,8 @@
                         <div class="relative w-56 mx-auto">
                             <div class="absolute rounded-l w-10 h-full flex items-center justify-center bg-gray-100 border text-gray-600 dark:bg-dark-1 dark:border-dark-4"> <i data-feather="calendar" class="w-4 h-4"></i> </div> <input type="text" class="datepicker form-control pl-12" data-single-mode="true">
                         </div>
-                        <div> <label for="regular-form-1" class="form-label">NILAI KONTRAK</label> <input id="regular-form-1" type="text" class="form-control" placeholder="Input nilai dalam Rupiah"> </div> <br>
+                        <div> <label for="regular-form-1" class="form-label">NILAI KONTRAK</label>
+                        <input id="regular-form-1" type="text" class="form-control" placeholder="Input nilai dalam Rupiah"> </div> <br>
                         <div> <label for="regular-form-1" class="form-label">NILAI SP2D</label>
                             <input id="regular-form-1" type="text" class="form-control" placeholder="Input nilai dalam Rupiah">
                             <form data-single="true" action="/file-upload" class="dropzone">
@@ -174,6 +175,58 @@
         </div>
         <!-- END: DATA PERUSAHAAN/DIREKTORAT -->
         <br>
+
+        <br>
+        <!-- BEGIN: MEDIA PRODUKSI -->
+        <div class="intro-y box">
+            <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
+                <h2 class="font-medium text-base mr-auto">
+                    PRODUKSI MEDIA
+                </h2>
+            </div>
+            <div id="vertical-form" class="p-5">
+                <div class="preview">
+                    <div class="mt-3">
+                        <label for="regular-form-4" class="form-label">Jenis Produksi</label>
+                        <div class="mt-2">
+                            <select data-placeholder="Select your favorite actors" class="tom-select w-full">
+                                <option value="1">Video Syuting Talent</option>
+                                <option value="2">Video CGI</option>
+                                <option value="3">Video Animasi 3D</option>
+                                <option value="4">Video Animasi 2D</option>
+                            </select>
+                        </div>
+                        <br>
+                        <div>
+                            <label for="regular-form-1" class="form-label">Jumlah Versi Produksi</label>
+                            <input id="regular-form-1" type="text" class="form-control" placeholder="Input nilai dalam Rupiah">
+                        </div>
+                        <br>
+                            <label for="regular-form-4" class="form-label">Lokasi Produksi</label>
+                        <div class="mt-2">
+                            <select data-placeholder="Select your favorite actors" class="tom-select w-full">
+                                <option value="1">Jakarta</option>
+                                <option value="2">Jawa Barat</option>
+                                <option value="3">Jawa Tengah</option>
+                                <option value="4">Jawa Timur</option>
+                            </select>
+                        </div>
+                        <br>
+                        <button class="btn btn-primary w-24 mr-1 mb-2">Add</button>
+                    </div>
+                </div>
+                <div class="source-code hidden">
+                    <button data-target="#copy-vertical-form" class="copy-code btn py-1 px-2 btn-outline-secondary"> <i data-feather="file" class="w-4 h-4 mr-2"></i> Copy example code </button>
+                    <div class="overflow-y-auto mt-3 rounded-md">
+                        <pre id="copy-vertical-form" class="source-preview"> <code class="text-xs p-0 rounded-md html pl-5 pt-8 pb-4 -mb-10 -mt-10"> HTMLOpenTagdivHTMLCloseTag HTMLOpenTaglabel for=&quot;vertical-form-1&quot; class=&quot;form-label&quot;HTMLCloseTagEmailHTMLOpenTag/labelHTMLCloseTag HTMLOpenTaginput id=&quot;vertical-form-1&quot; type=&quot;text&quot; class=&quot;form-control&quot; placeholder=&quot;example@gmail.com&quot;HTMLCloseTag HTMLOpenTag/divHTMLCloseTag HTMLOpenTagdiv class=&quot;mt-3&quot;HTMLCloseTag HTMLOpenTaglabel for=&quot;vertical-form-2&quot; class=&quot;form-label&quot;HTMLCloseTagPasswordHTMLOpenTag/labelHTMLCloseTag HTMLOpenTaginput id=&quot;vertical-form-2&quot; type=&quot;text&quot; class=&quot;form-control&quot; placeholder=&quot;secret&quot;HTMLCloseTag HTMLOpenTag/divHTMLCloseTag HTMLOpenTagdiv class=&quot;form-check mt-5&quot;HTMLCloseTag HTMLOpenTaginput id=&quot;vertical-form-3&quot; class=&quot;form-check-input&quot; type=&quot;checkbox&quot; value=&quot;&quot;HTMLCloseTag HTMLOpenTaglabel class=&quot;form-check-label&quot; for=&quot;vertical-form-3&quot;HTMLCloseTagRemember meHTMLOpenTag/labelHTMLCloseTag HTMLOpenTag/divHTMLCloseTag HTMLOpenTagbutton class=&quot;btn btn-primary mt-5&quot;HTMLCloseTagLoginHTMLOpenTag/buttonHTMLCloseTag </code> </pre>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END: Media PRODUKSI -->
+
+<br>
+
         <!-- BEGIN: MEDIA PLACEMENT -->
         <div class="intro-y box">
             <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
@@ -233,12 +286,16 @@
             </div>
             <div id="horizontal-form" class="p-5">
                 <div class="preview">
-                    <button class="btn btn-primary w-32 mr-2 mb-2"> <i data-feather="hard-drive" class="w-4 h-4 mr-2"></i> Simpan </button> <button class="btn btn-danger w-32 mr-2 mb-2"> <i data-feather="trash" class="w-4 h-4 mr-2"></i> Hapus </button> <button class="btn btn-warning w-32 mr-2 mb-2"> <i data-feather="book" class="w-4 h-4 mr-2"></i> New </button> <button class="btn btn-success w-32 mr-2 mb-2"> <i data-feather="printer" class="w-4 h-4 mr-2"></i> Print </button>
+                    <button class="btn btn-primary w-32 mr-2 mb-2"> <i data-feather="hard-drive" class="w-4 h-4 mr-2"></i> Simpan </button>
+                    <button class="btn btn-danger w-32 mr-2 mb-2"> <i data-feather="edit" class="w-4 h-4 mr-2"></i> Edit Data </button>
+                    <button class="btn btn-success w-32 mr-2 mb-2"> <i data-feather="printer" class="w-4 h-4 mr-2"></i> Print </button>
                 </div>
                 <div class="source-code hidden">
-                    <button data-target="#copy-horizontal-form" class="copy-code btn py-1 px-2 btn-outline-secondary"> <i data-feather="file" class="w-4 h-4 mr-2"></i> Copy example code </button>
+                    <button data-target="#copy-horizontal-form" class="copy-code btn py-1 px-2 btn-outline-secondary">
+                        <i data-feather="file" class="w-4 h-4 mr-2"></i> Copy example code </button>
                     <div class="overflow-y-auto mt-3 rounded-md">
-                        <pre id="copy-horizontal-form" class="source-preview"> <code class="text-xs p-0 rounded-md html pl-5 pt-8 pb-4 -mb-10 -mt-10"> HTMLOpenTagdiv class=&quot;form-inline&quot;HTMLCloseTag HTMLOpenTaglabel for=&quot;horizontal-form-1&quot; class=&quot;form-label sm:w-20&quot;HTMLCloseTagEmailHTMLOpenTag/labelHTMLCloseTag HTMLOpenTaginput id=&quot;horizontal-form-1&quot; type=&quot;text&quot; class=&quot;form-control&quot; placeholder=&quot;example@gmail.com&quot;HTMLCloseTag HTMLOpenTag/divHTMLCloseTag HTMLOpenTagdiv class=&quot;form-inline mt-5&quot;HTMLCloseTag HTMLOpenTaglabel for=&quot;horizontal-form-2&quot; class=&quot;form-label sm:w-20&quot;HTMLCloseTagPasswordHTMLOpenTag/labelHTMLCloseTag HTMLOpenTaginput id=&quot;horizontal-form-2&quot; type=&quot;password&quot; class=&quot;form-control&quot; placeholder=&quot;secret&quot;HTMLCloseTag HTMLOpenTag/divHTMLCloseTag HTMLOpenTagdiv class=&quot;form-check sm:ml-20 sm:pl-5 mt-5&quot;HTMLCloseTag HTMLOpenTaginput id=&quot;horizontal-form-3&quot; class=&quot;form-check-input&quot; type=&quot;checkbox&quot; value=&quot;&quot;HTMLCloseTag HTMLOpenTaglabel class=&quot;form-check-label&quot; for=&quot;horizontal-form-3&quot;HTMLCloseTagRemember meHTMLOpenTag/labelHTMLCloseTag HTMLOpenTag/divHTMLCloseTag HTMLOpenTagdiv class=&quot;sm:ml-20 sm:pl-5 mt-5&quot;HTMLCloseTag HTMLOpenTagbutton class=&quot;btn btn-primary&quot;HTMLCloseTagLoginHTMLOpenTag/buttonHTMLCloseTag HTMLOpenTag/divHTMLCloseTag </code> </pre>
+                        <pre id="copy-horizontal-form" class="source-preview">
+                            <code class="text-xs p-0 rounded-md html pl-5 pt-8 pb-4 -mb-10 -mt-10"> HTMLOpenTagdiv class=&quot;form-inline&quot;HTMLCloseTag HTMLOpenTaglabel for=&quot;horizontal-form-1&quot; class=&quot;form-label sm:w-20&quot;HTMLCloseTagEmailHTMLOpenTag/labelHTMLCloseTag HTMLOpenTaginput id=&quot;horizontal-form-1&quot; type=&quot;text&quot; class=&quot;form-control&quot; placeholder=&quot;example@gmail.com&quot;HTMLCloseTag HTMLOpenTag/divHTMLCloseTag HTMLOpenTagdiv class=&quot;form-inline mt-5&quot;HTMLCloseTag HTMLOpenTaglabel for=&quot;horizontal-form-2&quot; class=&quot;form-label sm:w-20&quot;HTMLCloseTagPasswordHTMLOpenTag/labelHTMLCloseTag HTMLOpenTaginput id=&quot;horizontal-form-2&quot; type=&quot;password&quot; class=&quot;form-control&quot; placeholder=&quot;secret&quot;HTMLCloseTag HTMLOpenTag/divHTMLCloseTag HTMLOpenTagdiv class=&quot;form-check sm:ml-20 sm:pl-5 mt-5&quot;HTMLCloseTag HTMLOpenTaginput id=&quot;horizontal-form-3&quot; class=&quot;form-check-input&quot; type=&quot;checkbox&quot; value=&quot;&quot;HTMLCloseTag HTMLOpenTaglabel class=&quot;form-check-label&quot; for=&quot;horizontal-form-3&quot;HTMLCloseTagRemember meHTMLOpenTag/labelHTMLCloseTag HTMLOpenTag/divHTMLCloseTag HTMLOpenTagdiv class=&quot;sm:ml-20 sm:pl-5 mt-5&quot;HTMLCloseTag HTMLOpenTagbutton class=&quot;btn btn-primary&quot;HTMLCloseTagLoginHTMLOpenTag/buttonHTMLCloseTag HTMLOpenTag/divHTMLCloseTag </code> </pre>
                     </div>
                 </div>
             </div>
