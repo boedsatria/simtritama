b5ce6a4e962c5f613064c9e47afb77042cabaa85
Rizkyrizt212 <75552121+Rizkyrizt212@users.noreply.github.com>
2021-10-29 21:35:02 +0700

    produksi

--- application/views/404.php
+++ application/views/404.php
@@ -1,39 +1,40 @@
 <!DOCTYPE html>
 <html lang="en" class="light">
-    <!-- BEGIN: Head -->
-    <head>
-        <meta charset="utf-8">
-        <link href="dist/images/logo.svg" rel="shortcut icon">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Rubick admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
-        <meta name="keywords" content="admin template, Rubick Admin Template, dashboard template, flat admin template, responsive admin template, web app">
-        <meta name="author" content="LEFT4CODE">
-        <title>Error Page</title>
-        <!-- BEGIN: CSS Assets-->
-        <link rel="stylesheet" href="dist/css/app.css" />
-        <!-- END: CSS Assets-->
-    </head>
-    <!-- END: Head -->
-    <body class="main">
-        <div class="container">
-            <!-- BEGIN: Error Page -->
-            <div class="error-page flex flex-col lg:flex-row items-center justify-center h-screen text-center lg:text-left">
-                <div class="-intro-x lg:mr-20">
-                    <img alt="Rubick Tailwind HTML Admin Template" class="h-48 lg:h-auto" src="dist/images/error-illustration.svg">
-                </div>
-                <div class="text-white mt-10 lg:mt-0">
-                    <div class="intro-x text-8xl font-medium">404</div>
-                    <div class="intro-x text-xl lg:text-3xl font-medium mt-5">Oops. Halaman yang anda cari tidak ditemukan.</div>
-                    <div class="intro-x text-lg mt-3">Halaman yang anda tuju mungkin belum tersedia atau anda tidak memiliki hak akses.</div>
-                    <button class="intro-x btn py-3 px-4 text-white border-white dark:border-dark-5 dark:text-gray-300 mt-10" onclick="window.location.href='index.html'">Kembali ke halaman utama</button>
-                </div>
+<!-- BEGIN: Head -->
+
+<head>
+    <meta charset="utf-8">
+    <link href="<?= base_url() ?>dist/images/logo.svg" rel="shortcut icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Rubick admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Rubick Admin Template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="LEFT4CODE">
+    <title>Error Page</title>
+    <!-- BEGIN: CSS Assets-->
+    <link rel="stylesheet" href="<?= base_url() ?>dist/css/app.css" />
+    <!-- END: CSS Assets-->
+</head>
+<!-- END: Head -->
+
+<body class="main">
+    <div class="container">
+        <!-- BEGIN: Error Page -->
+        <div class="error-page flex flex-col lg:flex-row items-center justify-center h-screen text-center lg:text-left">
+            <div class="-intro-x lg:mr-20">
+                <img alt="Rubick Tailwind HTML Admin Template" class="h-48 lg:h-auto" src="<?= base_url() ?>dist/images/error-illustration.svg">
             </div>
-            <!-- END: Error Page -->
+            <div class="text-white mt-10 lg:mt-0">
+                <div class="intro-x text-8xl font-medium">404</div>
+                <div class="intro-x text-xl lg:text-3xl font-medium mt-5">Oops. Halaman yang anda cari tidak ditemukan.</div>
+                <div class="intro-x text-lg mt-3">Halaman yang anda tuju mungkin belum tersedia atau anda tidak memiliki hak akses.</div>
+                <button class="intro-x btn py-3 px-4 text-white border-white dark:border-dark-5 dark:text-gray-300 mt-10" onclick="window.location.href='<?= base_url() ?>'">Kembali ke halaman utama</button>
+            </div>
         </div>
-        <!-- BEGIN: JS Assets-->
-        <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
-        <script src="https://maps.googleapis.com/maps/api/js?key=["your-google-map-api"]&libraries=places"></script>
-        <script src="dist/js/app.js"></script>
-        <!-- END: JS Assets-->
-    </body>
+        <!-- END: Error Page -->
+    </div>
+    <!-- BEGIN: JS Assets-->
+    <script src="<?= base_url() ?>dist/js/app.js"></script>
+    <!-- END: JS Assets-->
+</body>
+
 </html>
--- application/views/pic/pic_media_order.php
+++ application/views/pic/pic_media_order.php
@@ -19,47 +19,49 @@
             </div>
             <div class="p-5">
                 RINGKOS
- 
- <!-- BEGIN: Nested Select -->
- <div class="mt-3"> <label>Direktorat</label>
-     <div class="mt-2"> <select data-placeholder="Select your favorite actors" class="tom-select w-full">
-             <optgroup label="Kementerian A">
-                 <option value="1">Direktorat 1</option>
-                 <option value="2">Direktorat 2</option>
-                 <option value="3">Direktorat 3</option>
-                 <option value="4">Direktorat 4</option>
-                 <option value="5">Direktorat 5</option>
-             </optgroup>
-             <optgroup label="Kementerian B">
-                 <option value="1">Direktorat 1</option>
-                 <option value="2">Direktorat 2</option>
-                 <option value="3">Direktorat 3</option>
-                 <option value="4">Direktorat 4</option>
-                 <option value="5">Direktorat 5</option>
-             </optgroup>
-         </select> </div>
- </div>
- <!-- END: Nested Select -->
-  <!-- BEGIN: Nested Select -->
-  <div class="mt-3"> <label>Nomor Ringkos</label>
-     <div class="mt-2"> <select data-placeholder="Select your favorite actors" class="tom-select w-full">
-             <optgroup label="Januari">
-                 <option value="1">Ringkos A101</option>
-                 <option value="2">Ringkos A102</option>
-                 <option value="3">Ringkos A103</option>
-                 <option value="4">Ringkos A104</option>
-                 <option value="5">Ringkos A105</option>
-             </optgroup>
-             <optgroup label="Februari">
-                 <option value="1">Ringkos B001</option>
-                 <option value="2">Ringkos B002</option>
-                 <option value="3">Ringkos B003</option>
-                 <option value="4">Ringkos B004</option>
-                 <option value="5">Ringkos B005</option>
-             </optgroup>
-         </select> </div>
- </div>
- <!-- END: Nested Select -->
+
+                <!-- BEGIN: Nested Select -->
+                <div class="mt-3"> <label>Direktorat</label>
+                    <div class="mt-2">
+                        <select data-placeholder="Select your favorite actors" class="tom-select w-full">
+                            <optgroup label="Kementerian A">
+                                <option value="1">Direktorat 1</option>
+                                <option value="2">Direktorat 2</option>
+                                <option value="3">Direktorat 3</option>
+                                <option value="4">Direktorat 4</option>
+                                <option value="5">Direktorat 5</option>
+                            </optgroup>
+                            <optgroup label="Kementerian B">
+                                <option value="1">Direktorat 1</option>
+                                <option value="2">Direktorat 2</option>
+                                <option value="3">Direktorat 3</option>
+                                <option value="4">Direktorat 4</option>
+                                <option value="5">Direktorat 5</option>
+                            </optgroup>
+                        </select>
+                    </div>
+                </div>
+                <!-- END: Nested Select -->
+                <!-- BEGIN: Nested Select -->
+                <div class="mt-3"> <label>Nomor Ringkos</label>
+                    <div class="mt-2"> <select data-placeholder="Select your favorite actors" class="tom-select w-full">
+                            <optgroup label="Januari">
+                                <option value="1">Ringkos A101</option>
+                                <option value="2">Ringkos A102</option>
+                                <option value="3">Ringkos A103</option>
+                                <option value="4">Ringkos A104</option>
+                                <option value="5">Ringkos A105</option>
+                            </optgroup>
+                            <optgroup label="Februari">
+                                <option value="1">Ringkos B001</option>
+                                <option value="2">Ringkos B002</option>
+                                <option value="3">Ringkos B003</option>
+                                <option value="4">Ringkos B004</option>
+                                <option value="5">Ringkos B005</option>
+                            </optgroup>
+                        </select> </div>
+                </div>
+                <!-- END: Nested Select -->
 
             </div>
         </div>
@@ -75,14 +77,18 @@
             <div class="p-5">
                 WAKTU PEKERJAAN
                 <div class="mt-3"> <label for="regular-form-5" class="form-label">Mulai Produksi</label>
-                <input id="regular-form-5" type="text" class="form-control" placeholder="Otomatis dari database timeline Ringkos" disabled> </div>
+                    <input id="regular-form-5" type="text" class="form-control" placeholder="Otomatis dari database timeline Ringkos" disabled>
+                </div>
                 <div class="mt-3"> <label for="regular-form-5" class="form-label">Batas akhir Produksi</label>
-                <input id="regular-form-5" type="text" class="form-control" placeholder="Otomatis dari database timeline Ringkos" disabled> </div>
+                    <input id="regular-form-5" type="text" class="form-control" placeholder="Otomatis dari database timeline Ringkos" disabled>
+                </div>
                 <div class="mt-3"> <label for="regular-form-5" class="form-label">Mulai Placement</label>
-                <input id="regular-form-5" type="text" class="form-control" placeholder="Otomatis dari database timeline Ringkos" disabled> </div>
+                    <input id="regular-form-5" type="text" class="form-control" placeholder="Otomatis dari database timeline Ringkos" disabled>
+                </div>
                 <div class="mt-3"> <label for="regular-form-5" class="form-label">Batas akhir Placement</label>
-                <input id="regular-form-5" type="text" class="form-control" placeholder="Otomatis dari database timeline Ringkos" disabled> </div>
-                 
+                    <input id="regular-form-5" type="text" class="form-control" placeholder="Otomatis dari database timeline Ringkos" disabled>
+                </div>
+
             </div>
         </div>
         <!-- END: BLOCK 2 -->
@@ -97,9 +103,11 @@
             <div class="p-5">
                 HPS Pagu Anggaran Pekerjaan
                 <div class="mt-3"> <label for="regular-form-5" class="form-label">HPS Produksi</label>
-                <input id="regular-form-5" type="text" class="form-control" placeholder="HPS sesuai Pagu Pekerjaan Otomatis dari database Ringkos" disabled> </div>
+                    <input id="regular-form-5" type="text" class="form-control" placeholder="HPS sesuai Pagu Pekerjaan Otomatis dari database Ringkos" disabled>
+                </div>
                 <div class="mt-3"> <label for="regular-form-5" class="form-label">HPS Placement</label>
-                <input id="regular-form-5" type="text" class="form-control" placeholder="HPS sesuai Pagu Pekerjaan Otomatis dari database Ringkos" disabled> </div>
+                    <input id="regular-form-5" type="text" class="form-control" placeholder="HPS sesuai Pagu Pekerjaan Otomatis dari database Ringkos" disabled>
+                </div>
             </div>
         </div>
         <!-- END: BLOCK 3 -->
@@ -116,46 +124,48 @@
             <div class="p-5">
                 Konten Produksi
                 <div class="mt-3"> <label for="regular-form-5" class="form-label">Jumlah Versi</label>
-                <input id="regular-form-5" type="text" class="form-control" placeholder="jumlah versi produksi otomatis dari data Ringkos" disabled> </div>
+                    <input id="regular-form-5" type="text" class="form-control" placeholder="jumlah versi produksi otomatis dari data Ringkos" disabled>
+                </div>
                 <div class="mt-3"> <label for="regular-form-5" class="form-label">Wilayah Pekerjaan</label>
-                <input id="regular-form-5" type="text" class="form-control" placeholder="otomatis sesuai data Ringkos" disabled> </div>
-                 <!-- BEGIN: Nested Select -->
- <div class="mt-3"> <label>Tim Pelaksana Produksi</label>
-     <div class="mt-2"> <select data-placeholder="Select your favorite actors" class="tom-select w-full">
-             <optgroup label="Jakarta">
-                 <option value="1">TIM 1</option>
-                 <option value="2">TIM 2</option>
-                 <option value="3">TIM 3</option>
-                 <option value="4">TIM 4</option>
-                 <option value="5">TIM 5</option>
-             </optgroup>
-             <optgroup label="Jawa Barat">
-                 <option value="1">TIM 6</option>
-                 <option value="2">TIM 7</option>
-                 <option value="3">TIM 8</option>
-                 <option value="4">TIM 9</option>
-                 <option value="5">TIM 10</option>
-             </optgroup>
-         </select> </div>
- </div>
- <!-- END: Nested Select -->
-            <div class="mt-3"> <label>Upload Materi Produksi</label>
-                <form action="/file-upload" class="dropzone">
-                    <div class="fallback"> <input name="file" type="file" multiple/> </div>
-                    <div class="dz-message" data-dz-message>
-                        <div class="text-lg font-medium">Drop files here or click to upload.</div>
-                        <div class="text-gray-600"> This is just a demo dropzone. Selected files are <span class="font-medium">not</span> actually uploaded. </div>
-                    </div>
-                </form> 
-            </div><br>
+                    <input id="regular-form-5" type="text" class="form-control" placeholder="otomatis sesuai data Ringkos" disabled>
+                </div>
+                <!-- BEGIN: Nested Select -->
+                <div class="mt-3"> <label>Tim Pelaksana Produksi</label>
+                    <div class="mt-2"> <select data-placeholder="Select your favorite actors" class="tom-select w-full">
+                            <optgroup label="Jakarta">
+                                <option value="1">TIM 1</option>
+                                <option value="2">TIM 2</option>
+                                <option value="3">TIM 3</option>
+                                <option value="4">TIM 4</option>
+                                <option value="5">TIM 5</option>
+                            </optgroup>
+                            <optgroup label="Jawa Barat">
+                                <option value="1">TIM 6</option>
+                                <option value="2">TIM 7</option>
+                                <option value="3">TIM 8</option>
+                                <option value="4">TIM 9</option>
+                                <option value="5">TIM 10</option>
+                            </optgroup>
+                        </select> </div>
+                </div>
+                <!-- END: Nested Select -->
+                <div class="mt-3"> <label>Upload Materi Produksi</label>
+                    <form action="/file-upload" class="dropzone">
+                        <div class="fallback"> <input name="file" type="file" multiple /> </div>
+                        <div class="dz-message" data-dz-message>
+                            <div class="text-lg font-medium">Drop files here or click to upload.</div>
+                            <div class="text-gray-600"> This is just a demo dropzone. Selected files are <span class="font-medium">not</span> actually uploaded. </div>
+                        </div>
+                    </form>
+                </div><br>
 
 
-            <button class="btn btn-success w-32 mr-2 mb-2"> <i data-feather="printer" class="w-4 h-4 mr-2"></i> Print </button>
-            <button class="btn btn-warning w-32 mr-2 mb-2"> <i data-feather="send" class="w-4 h-4 mr-2"></i> Kirim </button>
+                <button class="btn btn-success w-32 mr-2 mb-2"> <i data-feather="printer" class="w-4 h-4 mr-2"></i> Print </button>
+                <button class="btn btn-warning w-32 mr-2 mb-2"> <i data-feather="send" class="w-4 h-4 mr-2"></i> Kirim </button>
 
 
-    </div>
-</div>
+            </div>
+        </div>
         <!-- END: BLOCK 4 -->
         <br>
         <!-- BEGIN: BLOCK 5 -->
@@ -168,58 +178,60 @@
             <div class="p-5">
                 MEDIA ORDER PLACEMENT
                 <div class="mt-3"> <label for="regular-form-5" class="form-label">Kode Produksi</label>
-                <input id="regular-form-5" type="text" class="form-control" placeholder="otomatis dari nomor surat order produksi" disabled> </div>
-                <div class="mt-3"> <label for="regular-form-5" class="form-label">Jumlah Versi</label>
-                <input id="regular-form-5" type="text" class="form-control" placeholder="jumlah versi produksi otomatis dari data Ringkos" disabled> </div>
-                <div class="mt-3"> <label for="regular-form-5" class="form-label">Media Placement</label>
-                <div class="overflow-x-auto">
-                    <table class="table table--sm">
-                        <thead>
-                            <tr>
-                                <th class="border-b-2 dark:border-dark-5 whitespace-nowrap">#</th>
-                                <th class="border-b-2 dark:border-dark-5 whitespace-nowrap">Jenis Media</th>
-                                <th class="border-b-2 dark:border-dark-5 whitespace-nowrap">Wilayah</th>
-                                <th class="border-b-2 dark:border-dark-5 whitespace-nowrap">Nama Media</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td class="border-b dark:border-dark-5">1</td>
-                                <td class="border-b dark:border-dark-5">TV Nasional</td>
-                                <td class="border-b dark:border-dark-5">Jakarta</td>
-                                <td class="border-b dark:border-dark-5">Trans7</td>
-                            </tr>
-                            <tr>
-                                <td class="border-b dark:border-dark-5">2</td>
-                                <td class="border-b dark:border-dark-5">TV Nasional</td>
-                                <td class="border-b dark:border-dark-5">Jakarta</td>
-                                <td class="border-b dark:border-dark-5">TVRI</td>
-                            </tr>
-                            <tr>
-                                <td class="border-b dark:border-dark-5">3</td>
-                                <td class="border-b dark:border-dark-5">Billboard</td>
-                                <td class="border-b dark:border-dark-5">Bandung</td>
-                                <td class="border-b dark:border-dark-5">Billboard Bandung 1</td>
-                            </tr>
-                            <tr>
-                                <td class="border-b dark:border-dark-5">4</td>
-                                <td class="border-b dark:border-dark-5">Billboard</td>
-                                <td class="border-b dark:border-dark-5">Semarang</td>
-                                <td class="border-b dark:border-dark-5">Billboard Semarang 1</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <input id="regular-form-5" type="text" class="form-control" placeholder="otomatis dari nomor surat order produksi" disabled>
                 </div>
-                <br>
-                <button class="btn btn-success w-32 mr-2 mb-2"> <i data-feather="printer" class="w-4 h-4 mr-2"></i> Print </button>
-                <button class="btn btn-warning w-32 mr-2 mb-2"> <i data-feather="send" class="w-4 h-4 mr-2"></i> Kirim </button>
+                <div class="mt-3"> <label for="regular-form-5" class="form-label">Jumlah Versi</label>
+                    <input id="regular-form-5" type="text" class="form-control" placeholder="jumlah versi produksi otomatis dari data Ringkos" disabled>
+                </div>
+                <div class="mt-3"> <label for="regular-form-5" class="form-label">Media Placement</label>
+                    <div class="overflow-x-auto">
+                        <table class="table table--sm">
+                            <thead>
+                                <tr>
+                                    <th class="border-b-2 dark:border-dark-5 whitespace-nowrap">#</th>
+                                    <th class="border-b-2 dark:border-dark-5 whitespace-nowrap">Jenis Media</th>
+                                    <th class="border-b-2 dark:border-dark-5 whitespace-nowrap">Wilayah</th>
+                                    <th class="border-b-2 dark:border-dark-5 whitespace-nowrap">Nama Media</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="border-b dark:border-dark-5">1</td>
+                                    <td class="border-b dark:border-dark-5">TV Nasional</td>
+                                    <td class="border-b dark:border-dark-5">Jakarta</td>
+                                    <td class="border-b dark:border-dark-5">Trans7</td>
+                                </tr>
+                                <tr>
+                                    <td class="border-b dark:border-dark-5">2</td>
+                                    <td class="border-b dark:border-dark-5">TV Nasional</td>
+                                    <td class="border-b dark:border-dark-5">Jakarta</td>
+                                    <td class="border-b dark:border-dark-5">TVRI</td>
+                                </tr>
+                                <tr>
+                                    <td class="border-b dark:border-dark-5">3</td>
+                                    <td class="border-b dark:border-dark-5">Billboard</td>
+                                    <td class="border-b dark:border-dark-5">Bandung</td>
+                                    <td class="border-b dark:border-dark-5">Billboard Bandung 1</td>
+                                </tr>
+                                <tr>
+                                    <td class="border-b dark:border-dark-5">4</td>
+                                    <td class="border-b dark:border-dark-5">Billboard</td>
+                                    <td class="border-b dark:border-dark-5">Semarang</td>
+                                    <td class="border-b dark:border-dark-5">Billboard Semarang 1</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <br>
+                    <button class="btn btn-success w-32 mr-2 mb-2"> <i data-feather="printer" class="w-4 h-4 mr-2"></i> Print </button>
+                    <button class="btn btn-warning w-32 mr-2 mb-2"> <i data-feather="send" class="w-4 h-4 mr-2"></i> Kirim </button>
 
 
 
+                </div>
             </div>
+            <!-- END: BLOCK 5 -->
         </div>
-        <!-- END: BLOCK 5 -->
-    </div>
 
     </div>
 </div>
