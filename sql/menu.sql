605b00f71534a056fc0fdced4e6608b327bfe5af
Rizkyrizt212 <75552121+Rizkyrizt212@users.noreply.github.com>
2021-11-02 13:52:24 +0700

    menu

--- sql/menu.sql
+++ sql/menu.sql
@@ -1,15 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 27, 2021 at 09:56 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.11
+-- Generation Time: Nov 02, 2021 at 07:50 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `simtritama`
@@ -39,8 +45,8 @@ CREATE TABLE `menu` (
 INSERT INTO `menu` (`id_menu`, `nama_menu`, `slug_menu`, `icon_menu`, `parent_menu`, `index_menu`, `role_menu`, `status_menu`) VALUES
 (1, 'Dashboard', 'dashboard', 'home', 0, 1, 0, 1),
 (2, 'divider', 'divider', '', 0, 2, 0, 1),
-(3, 'Master Data', '#', 'edit', 0, 3, 1, 1),
-(4, 'Data Client', 'masterdata/data_client', 'activity', 3, 1, 1, 1),
+(3, 'Master Data', '#', 'edit', 0, 3, 0, 1),
+(4, 'Data Client', 'masterdata/data_client', 'activity', 3, 1, 0, 1),
 (5, 'P I C', '#', 'user', 0, 5, 2, 1),
 (6, 'Data Project', 'pic/data_project', 'activity', 5, 1, 2, 1),
 (7, 'E-Faktur', 'pic/e_faktur', 'activity', 5, 2, 2, 1),
@@ -66,7 +72,8 @@ INSERT INTO `menu` (`id_menu`, `nama_menu`, `slug_menu`, `icon_menu`, `parent_me
 (27, 'divider', 'divider', '', 0, 6, 0, 1),
 (28, 'divider', 'divider', '', 0, 8, 0, 1),
 (29, 'divider', 'divider', '', 0, 10, 0, 1),
-(30, 'Data User', 'masterdata/data_user', 'activity', 3, 2, 1, 1);
+(30, 'Data User', 'masterdata/data_user', 'activity', 3, 2, 1, 1),
+(31, 'Data Media', 'masterdata/data_media', 'activity', 3, 3, 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -86,5 +93,10 @@ ALTER TABLE `menu`
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 COMMIT;
