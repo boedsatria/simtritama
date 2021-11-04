21b7862d072692bed1662074ca254a6bc00734a9
Rizkyrizt212 <75552121+Rizkyrizt212@users.noreply.github.com>
2021-11-04 22:14:30 +0700

    detail

--- application/views/masterdata/detail_penyelenggara.php
+++ application/views/masterdata/detail_penyelenggara.php
@@ -19,114 +19,159 @@
                 <div class="overflow-x-auto">
                     <table class="table">
                         <thead>
-                            <tr class="bg-gray-700 dark:bg-dark-1 text-white">
+                            <tr class="bg-gray-700 dark:bg-dark-1 text-white text-center">
                                 <th class="whitespace-nowrap">No.</th>
-                                <th class="whitespace-nowrap">Keterangan</th>
+                                <th class="whitespace-nowrap">Informasi</th>
                                 <th class="whitespace-nowrap">Data</th>
-                                <th class="whitespace-nowrap">View Image</th>
+                                <th class="whitespace-nowrap">Aksi</th>
                             </tr>
                         </thead>
                         <tbody>
+                            <!-- data perusahaan -->
                             <tr>
                                 <td class="border-b dark:border-dark-5">1</td>
                                 <td class="border-b dark:border-dark-5">Nama Perusahaan</td>
-                                <td class="border-b dark:border-dark-5">PT. Uhuy aja dah</td>
+                                <td class="border-b dark:border-dark-5">satriacorp.id</td>
                                 <td class="border-b dark:border-dark-5"> - </td>
                             </tr>
                             <tr>
                                 <td class="border-b dark:border-dark-5">2</td>
-                                <td class="border-b dark:border-dark-5">No.npwp</td>
-                                <td class="border-b dark:border-dark-5">12345678987yy65r43</td>
-                                <td class="border-b dark:border-dark-5">
-                                    <a href="<?= base_url('masterdata/detail_image') ?>" target="_blank" class="btn btn-success w-24 mr-2 mb-2">
-                                        <i data-feather="eye" class="w-4 h-4 mr-2"></i>
-                                        Lihat </a>
-                                </td>
+                                <td class="border-b dark:border-dark-5">Alamat</td>
+                                <td class="border-b dark:border-dark-5">Pondok Gede</td>
+                                <td class="border-b dark:border-dark-5"></td>
                             </tr>
+
+                            <!-- identitas pengurus -->
+
                             <tr>
                                 <td class="border-b dark:border-dark-5">3</td>
-                                <td class="border-b dark:border-dark-5">Alamat</td>
-                                <td class="border-b dark:border-dark-5">Jl. xxxxxx no xxxx</td>
+                                <td class="border-b dark:border-dark-5">Nama Komisaris</td>
+                                <td class="border-b dark:border-dark-5">Budi Hartono</td>
                                 <td class="border-b dark:border-dark-5"> - </td>
                             </tr>
                             <tr>
                                 <td class="border-b dark:border-dark-5">4</td>
-                                <td class="border-b dark:border-dark-5">Tanggal Akhir Tayang</td>
-                                <td class="border-b dark:border-dark-5">01 Maret 2022</td>
+                                <td class="border-b dark:border-dark-5">No. KTP komisaris</td>
+                                <td class="border-b dark:border-dark-5">123456789</td>
                                 <td class="border-b dark:border-dark-5"> - </td>
                             </tr>
                             <tr>
                                 <td class="border-b dark:border-dark-5">5</td>
-                                <td class="border-b dark:border-dark-5">Sub Bidang</td>
-                                <td class="border-b dark:border-dark-5">3</td>
+                                <td class="border-b dark:border-dark-5">Foto KTP komisaris</td>
+                                <td class="border-b dark:border-dark-5">-</td>
+                                <td class="border-b dark:border-dark-5">
+                                    <button class="btn btn-success w-24 mr-2 mb-2">Download</button>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td class="border-b dark:border-dark-5">6</td>
+                                <td class="border-b dark:border-dark-5">Nama Direktur</td>
+                                <td class="border-b dark:border-dark-5">Heru Poerwanto</td>
                                 <td class="border-b dark:border-dark-5"> - </td>
                             </tr>
                             <tr>
-                                <td class="border-b dark:border-dark-5">6</td>
-                                <td class="border-b dark:border-dark-5">Jenis Media</td>
-                                <td class="border-b dark:border-dark-5">Animasi 3D</td>
-                                <td class="border-b dark:border-dark-5"> 40 </td>
-                            </tr>
-                            <tr>
                                 <td class="border-b dark:border-dark-5">7</td>
-                                <td class="border-b dark:border-dark-5">Lokasi Media</td>
-                                <td class="border-b dark:border-dark-5">Bandar Lampung</td>
+                                <td class="border-b dark:border-dark-5">No. KTP Direktur</td>
+                                <td class="border-b dark:border-dark-5">123456789</td>
                                 <td class="border-b dark:border-dark-5"> - </td>
                             </tr>
                             <tr>
                                 <td class="border-b dark:border-dark-5">8</td>
-                                <td class="border-b dark:border-dark-5">Nama Media</td>
-                                <td class="border-b dark:border-dark-5">Animasi 2D</td>
-                                <td class="border-b dark:border-dark-5"> 40 </td>
+                                <td class="border-b dark:border-dark-5">Foto KTP Direktur</td>
+                                <td class="border-b dark:border-dark-5">-</td>
+                                <td class="border-b dark:border-dark-5">
+                                    <button class="btn btn-success w-24 mr-2 mb-2">Download</button>
+                                </td>
                             </tr>
+                            <!-- NPWP -->
                             <tr>
                                 <td class="border-b dark:border-dark-5">9</td>
-                                <td class="border-b dark:border-dark-5">Jenis Media</td>
-                                <td class="border-b dark:border-dark-5">Animasi 3D</td>
-                                <td class="border-b dark:border-dark-5"> 40 </td>
+                                <td class="border-b dark:border-dark-5">No. NPWP Perusahaan </td>
+                                <td class="border-b dark:border-dark-5">123456789</td>
+                                <td class="border-b dark:border-dark-5"> - </td>
                             </tr>
                             <tr>
                                 <td class="border-b dark:border-dark-5">10</td>
-                                <td class="border-b dark:border-dark-5">Lokasi Media</td>
-                                <td class="border-b dark:border-dark-5">Jakarta</td>
+                                <td class="border-b dark:border-dark-5">Image NPWP Perusahaan</td>
+                                <td class="border-b dark:border-dark-5">-</td>
+                                <td class="border-b dark:border-dark-5">
+                                    <button class="btn btn-success w-24 mr-2 mb-2">Download</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">9</td>
+                                <td class="border-b dark:border-dark-5">No. NPWP Komisaris </td>
+                                <td class="border-b dark:border-dark-5">123456789</td>
                                 <td class="border-b dark:border-dark-5"> - </td>
+                            </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">10</td>
+                                <td class="border-b dark:border-dark-5">Image NPWP Komisaris</td>
+                                <td class="border-b dark:border-dark-5">-</td>
+                                <td class="border-b dark:border-dark-5">
+                                    <button class="btn btn-success w-24 mr-2 mb-2">Download</button>
+                                </td>
                             </tr>
                             <tr>
                                 <td class="border-b dark:border-dark-5">11</td>
-                                <td class="border-b dark:border-dark-5">Nama Media</td>
-                                <td class="border-b dark:border-dark-5">Animasi 2D</td>
-                                <td class="border-b dark:border-dark-5"> 40 </td>
-                            </tr>
-                            <tr>
-                                <td class="border-b dark:border-dark-5">12</td>
-                                <td class="border-b dark:border-dark-5">Jenis Media</td>
-                                <td class="border-b dark:border-dark-5">Animasi 3D</td>
-                                <td class="border-b dark:border-dark-5"> 40 </td>
-                            </tr>
-                            <tr>
-                                <td class="border-b dark:border-dark-5">13</td>
-                                <td class="border-b dark:border-dark-5">Lokasi Media</td>
-                                <td class="border-b dark:border-dark-5">Bandung</td>
+                                <td class="border-b dark:border-dark-5">No. NPWP Direktur </td>
+                                <td class="border-b dark:border-dark-5">123456789</td>
                                 <td class="border-b dark:border-dark-5"> - </td>
                             </tr>
                             <tr>
-                                <td class="border-b dark:border-dark-5">14</td>
-                                <td class="border-b dark:border-dark-5">Nama Media</td>
-                                <td class="border-b dark:border-dark-5">Animasi 2D</td>
-                                <td class="border-b dark:border-dark-5"> 40 </td>
+                                <td class="border-b dark:border-dark-5">12</td>
+                                <td class="border-b dark:border-dark-5">Image NPWP Direktur</td>
+                                <td class="border-b dark:border-dark-5">-</td>
+                                <td class="border-b dark:border-dark-5">
+                                    <button class="btn btn-success w-24 mr-2 mb-2">Download</button>
+                                </td>
                             </tr>
                             <tr>
-                                <td class="border-b dark:border-dark-5">12</td>
-                                <td class="border-b dark:border-dark-5">Pagu Biaya Media</td>
-                                <td class="border-b dark:border-dark-5">450.000.000,00</td>
-                                <td class="border-b dark:border-dark-5"> Rupiah </td>
+                                <td class="border-b dark:border-dark-5">13</td>
+                                <td class="border-b dark:border-dark-5">Image SPT Bulanan</td>
+                                <td class="border-b dark:border-dark-5">-</td>
+                                <td class="border-b dark:border-dark-5">
+                                    <button class="btn btn-success w-24 mr-2 mb-2">Download</button>
+                                </td>
                             </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">14</td>
+                                <td class="border-b dark:border-dark-5">Image SPT Triwulan</td>
+                                <td class="border-b dark:border-dark-5">-</td>
+                                <td class="border-b dark:border-dark-5">
+                                    <button class="btn btn-success w-24 mr-2 mb-2">Download</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">15</td>
+                                <td class="border-b dark:border-dark-5">Image SPT Tahunan</td>
+                                <td class="border-b dark:border-dark-5">-</td>
+                                <td class="border-b dark:border-dark-5">
+                                    <button class="btn btn-success w-24 mr-2 mb-2">Download</button>
+                                </td>
+                            </tr>
+                            <!-- legalitas -->
+                            <tr>
+                                <td class="border-b dark:border-dark-5">16</td>
+                                <td class="border-b dark:border-dark-5">No. Akta Perusahaan</td>
+                                <td class="border-b dark:border-dark-5">123456789</td>
+                                <td class="border-b dark:border-dark-5"> - </td>
+                            </tr>
+                            <tr>
+                                <td class="border-b dark:border-dark-5">17</td>
+                                <td class="border-b dark:border-dark-5">Image Akta Perusahaan</td>
+                                <td class="border-b dark:border-dark-5">-</td>
+                                <td class="border-b dark:border-dark-5">
+                                    <button class="btn btn-success w-24 mr-2 mb-2">Download</button>
+                                </td>
+                            </tr>
+
                         </tbody>
                     </table>
 
                     <br>
-                    <button class="btn btn-primary w-48 mr-2 mb-2"> <i data-feather="printer" class="w-4 h-4 mr-2"></i> Print Data </button>
-                    <button class="btn btn-success w-48 mr-2 mb-2"> <i data-feather="hard-drive" class="w-4 h-4 mr-2"></i> Download Data </button>
+                    <button class="btn btn-primary w-48 mr-2 mb-2"> <i data-feather="printer" class="w-4 h-4 mr-2"></i> Print Rekap </button>
                     <a href="<?= base_url('masterdata/data_penyelenggara') ?>" class="btn btn-dark w-48 mr-2 mb-2"> <i data-feather="skip-back" class="w-4 h-4 mr-2"></i>
                         Back </a>
                 </div>
