7b83faa4b4e93666614903df22a863d58d4220f8
boedsatria <stribez@gmail.com>
2022-02-28 19:13:21 +0700

    fix

--- application/models/FinanceModel.php
+++ application/models/FinanceModel.php
@@ -7,18 +7,18 @@ class FinanceModel extends CI_Model
   function get($jenis = false, $search = false, $limit = false, $offset = false)
   {
     $this->db->from('petty_cash');
-    if ($search) $this->db->like('nama_project', $search, 'both');
-    if ($limit) $this->db->limit($limit, $offset);
-    if($jenis == 1):
-      $this->db->where('jenis_project', 1);
-      $this->db->or_where('jenis_project', 3);
-    elseif($jenis == 2):
-      $this->db->where('jenis_project', 2);
-      $this->db->or_where('jenis_project', 3);
-    else:
-      $this->db->where('jenis_project <=', $jenis);
-    endif;
-    $this->db->join('client', 'id_client = client_project', 'LEFT');
+    // if ($search) $this->db->like('nama_project', $search, 'both');
+    // if ($limit) $this->db->limit($limit, $offset);
+    // if($jenis == 1):
+    //   $this->db->where('jenis_project', 1);
+    //   $this->db->or_where('jenis_project', 3);
+    // elseif($jenis == 2):
+    //   $this->db->where('jenis_project', 2);
+    //   $this->db->or_where('jenis_project', 3);
+    // else:
+    //   $this->db->where('jenis_project <=', $jenis);
+    // endif;
+    // $this->db->join('client', 'id_client = client_project', 'LEFT');
     // $this->db->get();
     // print_r($this->db->last_query());die;
     return $this->db->get();
